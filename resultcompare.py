0a1afd5280f8b378f51c081637300ed399499fb8
Yongbo Wang <ybwang@n43.(none)>
2016-03-10 10:13:31 -0800

    2016-03-10

--- resultcompare.py
+++ resultcompare.py
@@ -11,8 +11,8 @@ def percolatorResult():
 	dirlist = os.listdir(cometout)
 	data = {}
 	for i in dirlist:
-		if i != 'GM18486.rna' and i != 'GM18909.rna':
-			continue
+		#if i != 'GM18486.rna' and i != 'GM18909.rna':
+			#continue
 		percolatorPath = cometout + i + '/0.05/*.0.05.pep.percolator'
 		identityPath = 'massDb/identity/identify_' + i + '.fa'
 		reg = 'chr'
@@ -25,7 +25,31 @@ def percolatorResult():
 			else:
 				data[t] = [i]
 	print data
+	num = {}
+	for t in data:
+		ele = data[t]
+		for i in range(len(ele)):
+			for j in range(i,len(ele)):
+				key1 = ele[i] + "\t" + ele[j]
+				key2 = ele[j] + "\t" + ele[i]
+				if key1 in num:
+					num[key1] = num[key1] + 1
+					num[key2] = num[key1]
+				else:
+					num[key1] = 1
+					num[key2] = num[key1]
 		
+	print "\t","\t".join(dirlist)
+	for i in dirlist:
+		print i,
+		for j in dirlist:
+			key = i + "\t" + j
+			n = 0
+			if key in num:
+				n = num[key]
+			print "\t",n,
+		print ""
+
 
 if __name__=='__main__':
         main()
